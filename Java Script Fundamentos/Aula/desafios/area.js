c9c0c29e64673b0a1ed972920dc4a2e3984fd19f
lucasdev-96 <lucasgodoidev96@gmail.com>
2021-09-28 20:59:16 -0300

    add exercise camelCase

--- Java Script Fundamentos/Aula/desafios/area.js
+++ Java Script Fundamentos/Aula/desafios/area.js
@@ -3,3 +3,26 @@ const raio = 10;
 const circumference = PI * raio ** 2;
 
 console.log(`A circunferencia é ${circumference}m2`);
+
+const array = ['lucas', 'pedro'];
+
+const empty = [];
+
+function fn() {
+  this.name = 'Lucas';
+  this.exec = () => console.log('lucas');
+  this.idade = 24;
+}
+
+const express = new fn();
+express.exec();
+console.log(express.name);
+console.log(express.idade);
+
+  const numberTabuada = ([primeiro, , segundo]) => {
+  console.log(primeiro,segundo)
+};
+
+
+
+numberTabuada(['lucas', 'jose', 'margarete']);
